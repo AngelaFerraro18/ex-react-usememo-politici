9f70500db0788a206b5b70c7acb55a29d0941795
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-07-11 12:49:21 +0200

    feat: milestone 1 completed, adding image placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,40 @@
+import { useEffect, useState } from "react"
 
 
 function App() {
 
+  const [politiciansList, setPoliticiansList] = useState([]);
+
+  useEffect(() => {
+    async function politiciansFetch() {
+
+      try {
+        const politicianPromise = await fetch(`http://localhost:3333/politicians`);
+        const politiciansData = await politicianPromise.json();
+        setPoliticiansList(politiciansData);
+      } catch (error) {
+        throw new Error('Errore nel caricamento della lista dei politici')
+      }
+    }
+
+    politiciansFetch();
+  }, []);
+
   return (
     <>
-      Ciao sono in app
+      <h1>Lista di politici:</h1>
+      <ul>
+        {politiciansList && politiciansList.map(politician => <li key={politician.id}><div>
+          <h3>Name: {politician.name}</h3>
+          <img src={politician.image || "/placeholder.jpg"} alt={politician.name}
+            onError={(e) => {
+              e.target.src = "/placeholder.jpg";
+            }} />
+          <p><strong>Position:</strong> {politician.position}</p>
+          <p><strong>Biography:</strong> {politician.biography}</p>
+        </div>
+        </li>)}
+      </ul>
     </>
   )
 }
